cb8e9be5281bf6263f42ff9811e832a354686b44
Alex Canario <alexcanario@gmail.com>
2024-08-13 15:46:04 -0300

    feature[infra]: add entities config, dbContext

---
+++ src/CleanArchMvc.Infra.Data/Context/AppDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace CleanArchMvc.Infra.Data.Context;
+
+internal class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+	public AppDbContext CreateDbContext(string[] args)
+	{
+		var currentDirectory = Directory.GetCurrentDirectory();
+		var currentDiParent = Directory.GetParent(currentDirectory);
+		var appSettingDir = $@"{currentDiParent!.FullName}\CookBook.Api\";
+
+		var configuration = new ConfigurationBuilder()
+			.SetBasePath(appSettingDir)
+			.AddJsonFile("appsettings.json")
+			.Build();
+
+		var builder = new DbContextOptionsBuilder<AppDbContext>();
+		var connectionString = configuration.GetConnectionString("Default");
+
+		builder.UseSqlServer(connectionString);
+
+		return new AppDbContext(builder.Options);
+	}
+}
---
+++ src/CleanArchMvc.Infra.IoC/ServicesCollection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchMvc.Infra.IoC;
+
+public static class ServicesCollection
+{
+	public static void AddRepositories(this IServiceCollection services)
+	{
+
+	}
+	
+}
