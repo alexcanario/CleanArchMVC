e239218d116821563f546fd505806ec28aae05e0
Alex Canario <alexcanario@gmail.com>
2024-08-16 16:35:05 -0300

    feature[app]: add product service

---
+++ src/CleanArchMvc.App/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchMvc.App.Dto;
+
+namespace CleanArchMvc.App.Interfaces;
+
+public interface IProductService
+{
+	Task<IEnumerable<ProductDto>> GetAllAsync();
+	Task<ProductDto> GetByIdAsync(int id);
+	Task<ProductDto> GetProductCategoryByIdAsync(int id);
+	Task Create(ProductDto productDto);
+	Task Update(ProductDto productDto);
+	Task Delete(int id);
+}
---
+++ src/CleanArchMvc.App/Services/ProductService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+
+using CleanArchMvc.App.Dto;
+using CleanArchMvc.App.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+
+namespace CleanArchMvc.App.Services;
+
+public class ProductService(IProductRepository productRepository, IMapper mapper) : IProductService
+{
+	public async Task<IEnumerable<ProductDto>> GetAllAsync()
+	{
+		return mapper.Map<IEnumerable<ProductDto>>(await productRepository.GetAllAsync()) ?? [];
+	}
+
+	public async Task<ProductDto> GetByIdAsync(int id)
+	{
+		return mapper.Map<ProductDto>(await productRepository.GetByIdAsync(id));
+	}
+
+	public async Task<ProductDto> GetProductCategoryByIdAsync(int id)
+	{
+		return mapper.Map<ProductDto>(await productRepository.GetProductWithCategoryByIdAsync(id));
+	}
+
+	public async Task Create(ProductDto productDto)
+	{
+		await productRepository.CreateAsync(mapper.Map<Product>(productDto));
+	}
+
+	public async Task Update(ProductDto categoryDto)
+	{
+		await productRepository.UpdateAsync(mapper.Map<Product>(categoryDto));
+	}
+
+	public async Task Delete(int id)
+	{
+		await productRepository.DeleteAsync(id);
+	}
+}
