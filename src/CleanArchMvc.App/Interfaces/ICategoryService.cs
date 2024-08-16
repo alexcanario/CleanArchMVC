3765e5418bf3b6365153392cbd33d99bbb8e6c17
Alex Canario <alexcanario@gmail.com>
2024-08-16 15:54:20 -0300

    feature[app]: add category service to app

---
+++ src/CleanArchMvc.App/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchMvc.App.Dto;
+
+namespace CleanArchMvc.App.Interfaces;
+
+public interface ICategoryService
+{
+	Task<IEnumerable<CategoryDto>> GetAllAsync();
+	Task<CategoryDto> GetCategoryByIdAsync(int id);
+	Task<CategoryDto> GetCategoryByNameAsync(string name);
+	Task CreateAsync(CategoryDto categoryDto);
+	Task UpdateAsync(CategoryDto categoryDto);
+	Task DeleteAsync(int id);
+}
---
+++ src/CleanArchMvc.App/Services/CategoryService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+
+using CleanArchMvc.App.Dto;
+using CleanArchMvc.App.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+
+namespace CleanArchMvc.App.Services;
+
+public class CategoryService(ICategoryRepository categoryRepository, IMapper mapper) : ICategoryService
+{
+	public async Task<IEnumerable<CategoryDto>> GetAllAsync()
+	{
+		return mapper.Map<IEnumerable<CategoryDto>>(await categoryRepository.GetAllAsync());
+	}
+
+	public async Task<CategoryDto> GetCategoryByIdAsync(int id)
+	{
+		return mapper.Map<CategoryDto>(await categoryRepository.GetByIdAsync(id));
+	}
+
+	public async Task<CategoryDto> GetCategoryByNameAsync(string name)
+	{
+		return mapper.Map<CategoryDto>(await categoryRepository.GetByNameAsync(name));
+	}
+
+	public async Task CreateAsync(CategoryDto categoryDto)
+	{
+		await categoryRepository.CreateAsync(mapper.Map<Category>(categoryDto));
+	}
+
+	public async Task UpdateAsync(CategoryDto categoryDto)
+	{
+		await categoryRepository.UpdateAsync(mapper.Map<Category>(categoryDto));
+	}
+
+	public async Task DeleteAsync(int id)
+	{
+		await categoryRepository.DeleteAsync(id);
+	}
+}
