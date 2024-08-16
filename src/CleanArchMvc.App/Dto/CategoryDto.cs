6628f9b0319bbe272e1c7e735c2f991c487ac370
Alex Canario <alexcanario@gmail.com>
2024-08-16 15:00:55 -0300

    feature[app]: add dtos and automapping

---
+++ src/CleanArchMvc.App/Dto/CategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.App.Dto;
+
+public record CategoryDto
+{
+	public int Id { get; init; }
+
+	[Required(ErrorMessage = "The NAME is required")]
+	[MinLength(3)]
+	[MaxLength(100)]
+	[DisplayName("Name")]
+	public string Name { get; init; } = string.Empty;
+}
