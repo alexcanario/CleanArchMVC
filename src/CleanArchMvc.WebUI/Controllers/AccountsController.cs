f9c4926596a80f596b8c368d0e2248313a69eb35
Alex Canario <alexcanario@gmail.com>
2024-09-11 20:23:54 -0300

    feat[account]: add controller

---
+++ src/CleanArchMvc.WebUI/Controllers/AccountsController.cs
@@ -0,0 +1,62 @@
+﻿using CleanArchMvc.Domain.Accounts;
+using CleanArchMvc.WebUI.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class AccountsController(IAuthenticate authentication) : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel(returnUrl));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel userLogin)
+        {
+            var result = await authentication.Login(userLogin.Email, userLogin.Password);
+
+            if (result)
+            {
+                if (string.IsNullOrEmpty(userLogin.ReturnUrl))
+                    return RedirectToAction("Index", "Home");
+
+                return Redirect(userLogin.ReturnUrl);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt. (Password must be strong).");
+                return View(userLogin);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel register)
+        {
+            var result = await authentication.Register(register.Email, register.Password);
+            if (result)
+            {
+                return Redirect("/");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid register attempt. (password must be strong)");
+                return View(register);
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await authentication.Logout();
+            return RedirectToAction("Login", "Accounts");
+        }
+    }
+}
---
+++ src/CleanArchMvc.WebUI/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace CleanArchMvc.WebUI.Models;
+
+public class LoginViewModel(string returnUrl)
+{
+    public LoginViewModel(string email, string password, string returnUrl) : this(returnUrl)
+    {
+        Email = email;
+        Password = password;
+    }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public string ReturnUrl { get; set; } = returnUrl;
+}
