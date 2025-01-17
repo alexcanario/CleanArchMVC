fb884eeeee5e4b28e03bfa94214886621079c7c7
Alex Canario <alexcanario@gmail.com>
2025-01-17 17:52:27 +0000

    feat: added project structure clean architecture

---
+++ src/CleanArchMVC.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CleanArchMVC.WebUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
