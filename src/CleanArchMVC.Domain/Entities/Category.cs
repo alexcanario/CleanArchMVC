83ba3fd9b3c5573ee936413fabcbb8928b284e1d
Alex Canario <alexcanario@gmail.com>
2025-01-25 19:23:09 +0000

    feat: Added category, product entity

---
+++ src/CleanArchMVC.Domain/Entities/Category.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMVC.Domain.Validation;
+
+namespace CleanArchMVC.Domain.Entities;
+
+public sealed class Category
+{
+    public Category(string name) => Validate(name);
+
+    public Category(int id, string? name)
+    {
+        Id = id;
+        Validate(name);
+    }
+
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public ICollection<Product>? Products { get; private set; }
+
+    private void Validate(string? name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), $"Invalid {name}, field is required.");
+        DomainExceptionValidation.When(name?.Length < 3, $"Invalid {name}, field is too short, minimum 3 characters.");
+        Name = name;
+    }
+}
---
+++ src/CleanArchMVC.Domain/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿namespace CleanArchMVC.Domain.Entities;
+
+public sealed class Product
+{
+    public Product(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public Product(string name) => Name = name;
+
+    public int Id { get; private set; }
+    public string Name { get; private set; } = string.Empty;
+    public string Description { get; private set; } = string.Empty;
+    public decimal Price { get; private set; }
+    public int Stock { get; private set; }
+    public string Image { get; private set; } = string.Empty;
+    public int CategoryId { get; private set; }
+    public Category? Category { get; private set; }
+
+    public void Update(int id, string name)
+    {
+        Id = id; Name = name;
+    }
+}
