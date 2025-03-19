de4c8f01ef7992267fe372abf1bce99bde2a5fd2
Alex Canario <alexcanario@gmail.com>
2025-03-19 17:00:06 +0000

    teste conn

---
+++ src/CleanArchMVC.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,13 @@
+namespace CleanArchMVC.Infra.IoC;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        
+
+        return services;
+    }8
+}
